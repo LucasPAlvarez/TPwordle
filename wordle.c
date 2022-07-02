0ad08a343cc67714e6a1e1528752e4cbabd4fd5e
LucasPAlvarez <alvarezarautlucas@gmail.com>
2022-07-01 23:21:49 -0300

    agregado funciones para abrir y cerrar el archivo de palabras

--- wordle.c
+++ wordle.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <errno.h>
 
 #ifndef  WORDLE_BASE
 #define WORDLE_BASE
@@ -10,4 +11,28 @@
 #include "wordleVisuals.h"
 #endif
 
+char *wordList[5]; 
+FILE *listaFile; 
 
+//habre el archivo de palabras a adivinar del wordle
+int openWordsFile (){
+    if((listaFile = fopen("PALABAS-5LETRAS.txt", "r")) == NULL){
+        printf("errno = %d\n", errno);
+        return 0;
+    }else{
+        printf("archivo existe\n");
+        return 1;
+    }
+}
+
+//cierra el archivo de palabras a adivinar del wordle
+void closeWordsFile (){
+    fclose(listaFile);
+}
+
+int main(){
+    int result = openWordsFile();
+    if(result){
+        closeWordsFile();
+    }
+}
---
+++ wordleLogic.h
@@ -0,0 +1,2 @@
+int openWordsFile ();
+void closeWordsFile ();
