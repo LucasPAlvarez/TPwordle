f99a1fbf8f6a6698e530e2eac95776b983bf99ba
LucasPAlvarez <alvarezarautlucas@gmail.com>
2022-07-03 23:57:50 -0300

    agregado el paametro tamaño de una palabra

--- parameters.h
+++ parameters.h
@@ -6,3 +6,6 @@
 #define YELLOW "\033[1;43;30m"
 #define GREEN "\033[1;42;30m"
 #define DEFAULT "\033[0m"
+
+//tamaño de la palabra en el juego (aunque sea 5 letras necesita el numero 7)
+#define WORD_LENGTH 7
