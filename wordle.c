0772221d5b8e1ca771dcf53c4097328aabe3b8f7
LucasPAlvarez <alvarezarautlucas@gmail.com>
2022-07-06 21:03:35 -0300

    aderido systema de multiples partidas y puntuacion ademas de bugfixes

--- wordle.c
+++ wordle.c
@@ -16,9 +16,17 @@
 char WordToGuess[WORD_LENGTH]; 
 char WordPlayer[WORD_LENGTH];
 
-char GridWordStorege[COLS][ROWS];
+// graba los espacios que fueron encontrados
+int foundSpots[5];
+
+int scoore;
+int result[5];
+
+char gridWordStorege[COLS][ROWS];
 char *GridColorStorege[COLS][ROWS];
-FILE *listaFile; 
+FILE *listaFile;
+
+int partActual, partTotal;
 
 //habre el archivo de palabras a adivinar del wordle
 int openWordsFile (){
@@ -47,6 +55,12 @@ void getRandWord(){
 //imprime la palabra guardada
 void printWord(){
     printf("%s\n", WordToGuess);
+}
+//elimina los restos en caso de un stdin largo 
+void garbegeCollector(){
+    char *garbege;
+    size_t len = 0;
+    getline(&garbege, &len, stdin);
 }
 
 //toma una palabra requiriendo que sea de 5 letras y combirtiendola en mayuscula
@@ -78,14 +92,19 @@ void wordToUpper(char *palabra){
     }
 }
 
+void resInit(){
+    for(int i = 0; i<5; i++){
+        result[i] = -1;
+    }
+}
+
 //compara la palabra del jugador y guarda el resultado
 int *CheckPlayerGuess(int turn){
-    static int result[5] = {-1,-1,-1,-1,-1};
     
     for(int i = 0; i<5; i++){
         if(WordPlayer[i] == WordToGuess[i]){
             GridColorStorege[turn-1][i] = GREEN;
-            GridWordStorege[turn-1][i] = WordPlayer[i];
+            gridWordStorege[turn-1][i] = WordPlayer[i];
             result[i] = LETTERCORRECT;
             continue;
         }
@@ -99,7 +118,7 @@ int *CheckPlayerGuess(int turn){
         if(result[i] == -1){
             result[i] = LETTERMISSING;
         }
-        GridWordStorege[turn-1][i] = WordPlayer[i];
+        gridWordStorege[turn-1][i] = WordPlayer[i];
     }
 
     return result;
@@ -118,17 +137,63 @@ int checkResult(int *result){
 void initializeGrid(){
     for (int i = 0; i < COLS; i++){
         for(int j = 0; j < ROWS; j++){
-            GridWordStorege[i][j] = '*';
+            gridWordStorege[i][j] = '*';
             GridColorStorege[i][j] = DEFAULT;
         }
     }
 }
 
+//inicializa foundSpots
+void FSstart(){
+    for(int i = 0; i<5; i++){
+        foundSpots[i] =1;
+    }
+}
+
+//inicializa y resetea el scoore a 5000
+void scooreStart(){
+    scoore = 5000;
+}
+
+void calculateScore(int *result, int turn){
+    if(checkResult(result)){
+        if(turn == 1){
+            scoore = 10000;
+        }else{
+            scoore += 2000;
+        }
+
+    }else{    
+        
+        //calculate new scoore
+        //decrece por el intento usado
+        scoore -= 500; 
+
+        for(int i = 0; i<5; i++){
+            scoore += (result[i] * 50) * foundSpots[i];
+            if(result[i] == LETTERCORRECT){
+                foundSpots[i] = 0;
+            }
+        }
+    }
+}
+
+int PedirCantPartidas(){
+    scanf("%d",&partTotal);
+    if(partTotal <= 8 && partTotal > 0){
+        partActual = 1;
+        return 1;
+    }
+    return 0;
+}
+
+// -------------- funciones visuales -------------------
+
 //imprime l grilla
 void printGrid(){
     for (int i = 0; i < COLS; i++){
         for(int j = 0; j < ROWS; j++){
-            printf("%s %c %s", GridColorStorege[i][j], GridWordStorege[i][j], DEFAULT);
+            printf("%s %c %s", GridColorStorege[i][j], gridWordStorege[i][j], DEFAULT);
         }
         printf("\n");
     }
@@ -137,7 +202,7 @@ void printGrid(){
 //pide al jugaro que ingrese una palabra de 5 letras
 void askForPlayerInput(){
     while(1){
-        printf("Ingrese una palabra de 5 letras: ");
+        printf("\nIngrese una palabra de 5 letras: ");
         if(getPlayerInput()){
             break;
         }
@@ -150,36 +215,85 @@ void ClearScreen(){
     system("clear");
 }
 
-//---------------------------------------------------
-
-//borrar despues
-void recordatorio(){
-    if(1){
-        printf("entra\n");
+void InitialMenu(){
+    printf("Bienvenidos al juego de Wordle.\n\nCUantas partidas desea jugar (el maximo son 8):");
+    while(1){
+        if(PedirCantPartidas()){
+            break;
+        }
+        printf("Debe ingresar unnumero entre 0 y 8\nIntentelo nuevamente:");
     }
+}
+
+void printHeadder(int turno){
+    printf("Partida %d de %d            Turno %d\n\n", partActual, partTotal, turno);
+}
+
+int askContinuar(){
+    while(1){
+        char resp;
+        printf("\nDesea continuar jugando[Y/N]: ");
+        scanf("%c", &resp);
+        if(resp == 'Y' || resp == 'y'){
+            return 1;
+        }
+        if(resp == 'N' || resp == 'n'){
+            return 0;
+        }
+        printf("\nDeve ingresar Y para si o N para no. Porfavor intentelo de nuevo");
+    }
+}
+
+void printScoore(){
+    printf("Scoore: %d\n\n", scoore);
 }
 
 // ------- game ----------
 void playGame(){
+    int again = 1;
+    InitialMenu();
+    do{
+        playRound();
+        if(partActual == partTotal){
+            break;
+        }
+        garbegeCollector();
+        again = askContinuar();
+        partActual++;
+    }while(again);
+
+}
+
+
+void playRound(){
     //initialize variables
     int turn = 1;
-    int *result; 
     getRandWord();
     initializeGrid();
-    
+    scooreStart();
+    FSstart();
+    resInit();
     do{
         //initialize visuals
         ClearScreen();
+        printHeadder(turn);
+        printScoore();
         printGrid();
         askForPlayerInput();
 
         //compare the input
-        result = CheckPlayerGuess(turn);
+        CheckPlayerGuess(turn);
+
+        calculateScore(result, turn);
 
         if(checkResult(result)){
             //print wining message and exit loop
             ClearScreen();
-            printf("--- Felicidades ---\n\nAdivinaste la palabras.\n\n");
+            printHeadder(turn);
+            printScoore();
+            printf("--- Felicidades ---\n\nAdivinaste la palabra. Era ");
+            printWord();
+            printf("\n\n");
             break;
         }
 
@@ -188,7 +302,10 @@ void playGame(){
     if(turn == 6){
         ClearScreen();
         //print losing message and show the word
+        printHeadder(turn);
+        printScoore();
         printf("--- Perdiste ---\n\nNo Adivinaste, la palabra era: %s\n\n",WordToGuess);
     }
     printGrid();
+    
 }
